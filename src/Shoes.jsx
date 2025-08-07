249855adcc634d0fe0863f148822f40c0c2bf0ac
berkal749 <berkalachrefpro@gmail.com>
2025-08-07 11:42:01 +0100

    fixed paths

--- src/Shoes.jsx
+++ src/Shoes.jsx
@@ -4,29 +4,29 @@ function Shoes() {
       <div className=" flex  h-14/17 w-17/19">
         <img
           className="h-full w-full rounded-lg"
-          src="src/assets/Shoes/Shoes1XL.png"
+          src="/Shoes/Shoes1XL.png"
           alt="Shoses-display"
         />
       </div>
       <div className="flex  pr-3 justify-center w-9/11 h-2/16">
         <img
           className="h-11/11 ml-5 rounded-lg"
-          src="src/assets/Shoes/Shoes4.png"
+          src="/Shoes/Shoes4.png"
           alt=""
         />
         <img
           className="h-11/11 ml-5 rounded-lg"
-          src="src/assets/Shoes/Shoes3.png"
+          src="/Shoes/Shoes3.png"
           alt=""
         />
         <img
           className="h-11/11 ml-5 rounded-lg"
-          src="src/assets/Shoes/Shoes2.png"
+          src="/Shoes/Shoes2.png"
           alt=""
         />
         <img
           className="h-11/11 ml-5 rounded-lg"
-          src="src/assets/Shoes/Shoes1.png"
+          src="/Shoes/Shoes1.png"
           alt=""
         />
       </div>
