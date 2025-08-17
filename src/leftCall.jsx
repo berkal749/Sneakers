b391e7dc3d9a09e40883c49fc4bc32df9029d1d5
berkal749 <berkalachrefpro@gmail.com>
2025-08-17 16:15:41 +0100

    fix: clean up formatting and indentation in leftCall component

--- src/leftCall.jsx
+++ src/leftCall.jsx
@@ -9,42 +9,42 @@ function leftCall() {
       <div className="mb-8">
         <div className="flex">
           <img className="mr-2" src="public/icons/mail.svg" alt="mail.svg" />
-          <p  className="font-bold">Email</p>
+          <p className="font-bold">Email</p>
         </div>
         <p className="ml-7.5 text-neutral-500 ">support@sneakerstore.com</p>
       </div>
 
-       <div className="mb-8">
+      <div className="mb-8">
         <div className="flex ">
           <img className="mr-2" src="public/icons/phone.svg" alt="phone.svg" />
-          <p  className="font-bold">Phone</p>
+          <p className="font-bold">Phone</p>
         </div>
         <p className="ml-7.5 text-neutral-500">+1 234 567 8900</p>
       </div>
 
-       <div className="mb-8" >
-        <div    className="flex   ">
-          <img  className="mr-2" src="public/icons/map-pin.svg" alt="map-pin.svg" />
-          <p  className="font-bold">Adress</p>
+      <div className="mb-8">
+        <div className="flex   ">
+          <img
+            className="mr-2"
+            src="public/icons/map-pin.svg"
+            alt="map-pin.svg"
+          />
+          <p className="font-bold">Adress</p>
         </div>
-        <p className="ml-7.5 text-neutral-500" >123 Sneaker Street, Shoe City, Country</p>
+        <p className="ml-7.5 text-neutral-500">
+          123 Sneaker Street, Shoe City, Country
+        </p>
       </div>
-      <div className="flex flex-col gap-3" >
+      <div className="flex flex-col gap-3">
         <p className="text-2xl font-bold">Follow Us</p>
 
         <div className="flex gap-3">
-<img src="/icons/facebook.svg" alt="facebook.svg" />
-<img src="public/icons/pen-off.svg" alt="X.svg" />
-<img src="/icons/instagram.svg" alt="instagram.svg" />
-<img src="public/icons/linkedin.svg" alt="linkdin.svg" />
-
+          <img src="/icons/facebook.svg" alt="facebook.svg" />
+          <img src="public/icons/pen-off.svg" alt="X.svg" />
+          <img src="/icons/instagram.svg" alt="instagram.svg" />
+          <img src="public/icons/linkedin.svg" alt="linkdin.svg" />
         </div>
-
-
-
       </div>
-
-      
     </div>
   );
 }
