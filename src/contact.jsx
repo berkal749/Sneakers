48b1d67576195833d5c87001498e8ffa4c1c4aff
berkal749 <berkalachrefpro@gmail.com>
2025-08-18 09:54:00 +0100

    refactor: remove unused import statements in Contact component

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,9 +1,8 @@
 import Nav from "./nav.jsx";
-import Head from "./head.jsx";
 import Footer from "./Footer.jsx";
 import Calls from "./Calls.jsx";
 import BigShoes from "./Bigshoes.jsx"
-import { Link } from "react-router";
+
 function Contact() {
   return (
     <div className="h-screen w-screen">
